504c69cc46b92a829f1512e527cd1039a0105ac6
Dávid <corten.mgs@gmail.com>
2017-08-18 11:42:03 +0200

    - Added 'Versenyszabályzat'

--- js/sign-up.js
+++ js/sign-up.js
@@ -3,15 +3,19 @@ document.querySelector('.sign-up-form').addEventListener('submit', (e) => {
     let submission = {
         name: e.target[0].value,
         contact: e.target[1].value,
-        horsename: e.target[2].value,
-        company: e.target[3].value
+        location: e.target[2].value,
+        horsename: e.target[3].value,
+        horsetype: e.target[4].value,
+        company: e.target[5].value
     };
+    
+    console.log(submission);
     axios.post('https://lovastalalkozo.herokuapp.com/signup', submission)
         .then((response) => {
             document.querySelector('.form-message').innerHTML = response.data.message;
 
             //Reset form
-            for(let i = 0; i < 4; i++){
+            for(let i = 0; i < 6; i++){
                 e.target[i].value = '';
             }
         })
